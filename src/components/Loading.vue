a2ca4171b9d5b37c62f5aee5c26c7bd9c7b7e59d
Lauri Hintsala <lauri.hintsala@verkkopaja.fi>
2022-01-27 22:11:29 +0200

    ui fixes

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="loading" class="inset-0 fixed z-99 bg-gray-900 text-gray-100 text-center p-10 opacity-90">
+  <div v-if="loading" class="inset-0 fixed z-99 bg-gray-900 text-gray-100 text-center p-10 bg-opacity-80">
     <div>
       Setting up python environment
     </div>
