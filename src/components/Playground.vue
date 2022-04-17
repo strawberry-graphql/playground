2b5ebb2883ecfa06f81d233734abde60ad7e2a8e
Patrick Arminio <patrick.arminio@gmail.com>
2022-04-16 23:25:33 -0500

    Initial implementation of fetching and storing on GitHub gists

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -1,52 +1,81 @@
 <template>
-  <div class="flex flex-col flex-wrap items-stretch border-0 border-b-1 border-red-700">
-    <Container
-      title="Strawberry Schema">
-      <CodeMirror
-        mode="python"
-        v-model:value="state.code"
-        :indent="4"
-      />
-    </Container>
-    <Container
-      title="GraphQL Query">
-      <CodeMirror
-        v-model:value="state.query"
-      />
-    </Container>
-    <Container
-      title="Query Variables"
-      :visible="false">
-      <CodeMirror
-        mode="json"
-        v-model:value="state.variables"
-      />
-    </Container>
+  <div v-if="!loading" class="absolute inset-0 flex flex-col font-sans">
+    <Header :state="state" />
+    <div class="flex flex-col flex-wrap flex-1 items-stretch border-0 border-b-1 border-red-700">
+      <Container title="Strawberry Schema">
+        <CodeMirror mode="python" v-model:value="data.code" :indent="4" />
+      </Container>
+      <Container title="GraphQL Query">
+        <CodeMirror v-model:value="data.query" />
+      </Container>
+      <Container title="Query Variables" :visible="false">
+        <CodeMirror mode="json" v-model:value="data.variables" />
+      </Container>
 
-    <div class="sm:w-1/2 h-0"/>
-    <div class="sm:flex-break border-0 sm:border-r-1 border-red-700" />
+      <div class="sm:w-1/2 h-0" />
+      <div class="sm:flex-break border-0 sm:border-r-1 border-red-700" />
 
-    <Container
-      title="Query Results">
-      <CodeMirror
-        mode="json"
-        :value="state.results"
-        :readonly="true"
-      />
-    </Container>
-    <Container title="GraphQL Schema">
-      <CodeMirror
-        mode="json"
-        :value="state.schema"
-        :readonly="true"
-      />
-    </Container>
+      <Container title="Query Results">
+        <CodeMirror mode="json" :value="results" :readonly="true" />
+      </Container>
+      <Container title="GraphQL Schema">
+        <CodeMirror mode="json" :value="schema" :readonly="true" />
+      </Container>
 
-    <div class="sm:w-1/2 h-0"/>
+      <div class="sm:w-1/2 h-0" />
+    </div>
+
+    <ErrorContainer class="max-h-5em sm:max-h-10em" :state="state" />
+    <!-- TODO: bring back loading, but keep the UI working -->
+    <!-- <Loading :loading="loading" /> -->
   </div>
 </template>
 
 <script setup>
-const props = defineProps(['state'])
-const state = reactive(props.state)
+import { useQuery } from '@urql/vue';
+import { useStrawberry } from '../utils/strawberry.js'
+import sampleRequirements from '../samples/requirements.txt?raw'
+import sampleCode from '../samples/schema.py?raw'
+import sampleQuery from '../samples/query.gql?raw'
+import sampleVariables from '../samples/variables.json?raw'
+
+const { id } = defineProps(['id'])
+
+console.log(sampleRequirements)
+
+const state = {}
+
+const result = useQuery({
+  query: `
+    query GetGist($id: ID!) {
+      gist(id: $id) {
+        id
+        query
+        code: schema
+        variables
+        requirements
+      }
+    }
+  `,
+  variables: { id },
+  pause: !id,
+});
+
+const loading = id ? result.fetching : false
+const data = ref({
+  id: null,
+  query: sampleQuery,
+  code: sampleCode,
+  variables: JSON.parse(sampleVariables),
+  requirements: sampleRequirements,
+})
+
+watch(result.data, (d) => {
+  data.value = d.gist
+})
+
+const { init, schema, results } = useStrawberry(data)
+
+// TODO: listen to version update
+init()
 </script>
